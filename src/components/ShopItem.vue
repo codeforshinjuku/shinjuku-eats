c1433a71f739f4b33aaab9759c9008743c7cc2b2
Yohei Ito <ito@smcr.jp>
2020-05-12 21:20:58 +0900

    画像の表示を配列から文字列に。

--- src/components/ShopItem.vue
+++ src/components/ShopItem.vue
@@ -6,9 +6,7 @@
         :to="{ name: 'shop_show', params: { name: name } }"
         :title="name + 'の詳細ページに移動します'"
         class="shop_link"
-      >
-        {{ name }}
-      </router-link>
+      >{{ name }}</router-link>
       <span v-else>{{ name }}</span>
     </h2>
     <div class="shop__tags">
@@ -21,17 +19,8 @@
       <p v-if="shopHoliday" class="shopHoliday">休業日：{{ shopHoliday }}</p>
     </div>
     <div class="shop__thumbnail">
-      <img
-        v-if="images[0]"
-        :src="images[0]"
-        :alt="'画像：' + name"
-        class="image"
-      />
-      <img
-        v-else
-        :src="'/images/placeholder' + category + '.png'"
-        class="image"
-      />
+      <img v-if="images != null" :src="images" :alt="'画像：' + name" class="image" />
+      <img v-else :src="'/images/placeholder' + category + '.png'" class="image" />
     </div>
     <div class="shop__description">{{ description }}</div>
     <div class="shop__icons">
@@ -66,9 +55,7 @@
             d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"
           />
         </svg>
-        <a :href="'tel:' + phone">
-          {{ reshapePhoneChar(phone) }}
-        </a>
+        <a :href="'tel:' + phone">{{ reshapePhoneChar(phone) }}</a>
       </div>
       <div class="icon share" @click="handleShareModal">
         <svg style="width:24px;height:24px" viewBox="0 0 24 24">
@@ -82,7 +69,8 @@
     </div>
     <div v-if="menu" class="shop__menu">
       <div class="menu_list" :class="!isMenu && menu.length > 50 ? 'hide' : ''">
-        {{ menu }}<br />
+        {{ menu }}
+        <br />
         {{ discount }}
       </div>
       <div
